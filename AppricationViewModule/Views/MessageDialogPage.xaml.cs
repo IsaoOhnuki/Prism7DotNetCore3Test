a073fb20496ee8b848182bbfbb78a7c7cfab212b
IsaoOhnuki <isa0_net@hotmail.com>
2021-12-11 21:34:53 +0900

    commit

---
+++ AppricationViewModule/Views/MessageDialogPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace AppricationViewModule.Views
+{
+    /// <summary>
+    /// Interaction logic for MessageDialogPage
+    /// </summary>
+    public partial class MessageDialogPage : UserControl
+    {
+        public MessageDialogPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
