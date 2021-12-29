f7973ff1cc7cd7b8e018cf869fc5163002a4b276
勲 大貫 <isa0_net@hotmail.com>
2021-12-29 20:43:52 +0900

    commit

--- DBMaigration/AppDbContext.cs
+++ DBMaigration/AppDbContext.cs
@@ -7,12 +7,62 @@ namespace DBMaigration
 {
     public class AppDbContext : DbContext
     {
+        /// <summary>
+        /// マイグレーションするクラスをDbSetで定義する。
+        /// </summary>
+        public DbSet<Table1> Table1 { get; set; }
+
+        /// <summary>
+        /// DbContext継承クラスを作りOnConfiguringをオーバーライドするとマイグレーションが機能する。
+        /// </summary>
+        /// <param name="optionsBuilder"></param>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            string server = "localhost\\SQLEXPRESS";
+            string database = "AppDb";
+            string user = "sa";
+            string pass = "Express";
+
+            _ = optionsBuilder.UseSqlServer("Persist Security Info=False;User ID=" + user + ";Password=" + pass + ";Initial Catalog=" + database + ";Server=" + server);
+        }
+    }
+}
+
+/*
+
+① EntityFrameworkCoreのインストール
+・Install-Package Microsoft.EntityFrameworkCore -Version 5.0.13
+・Install-Package Microsoft.EntityFrameworkCore.Tools -Version 5.0.13
+
+② DbContextクラスの作成
+    public class AppDbContext : DbContext
+    {
         public DbSet<Table1> Table1 { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
-            _ = optionsBuilder.UseSqlServer("Persist Security Info=False;User ID=sa;Password=Express;Initial Catalog=AppDb;Server=localhost\\SQLEXPRESS");
+            _ = optionsBuilder.UseSqlServer("Persist Security Info=False;User ID=sa;Password=Express;Initial Catalog=AppDb;Server=DESKTOP-II14AQP\\SQLEXPRESS");
         }
     }
-}
+
+③ マイグレーション開始
+・Add-Migration AppDatabase　←　マイグレーションクラス名
+
+④ DBServerに反映する
+・Update-Database
+
+⑤ マイグレーション更新
+・Add-Migration AppDatabase2　←　新しいマイグレーションクラス名
+
+⑥ 更新をDBServerに反映する
+・Update-Database AppDatabase2
+
+⑦ DBServerをロールバックする
+・Update-Database AppDatabase2
+
+⑧ マイグレーションを初期化するにはMigrationsフォルダとデータベースを削除して③から始める
+
+ */
--- DBMaigration/MainWindow.xaml.cs
+++ DBMaigration/MainWindow.xaml.cs
@@ -26,39 +26,3 @@ namespace DBMaigration
         }
     }
 }
-
-/*
-
-① EntityFrameworkCoreのインストール
-・Install-Package Microsoft.EntityFrameworkCore -Version 5.0.13
-
-② DbContextクラスの作成
-    public class AppDbContext : DbContext
-    {
-        public DbSet<Table1> Table1 { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            base.OnConfiguring(optionsBuilder);
-            _ = optionsBuilder.UseSqlServer("Persist Security Info=False;User ID=sa;Password=Express;Initial Catalog=AppDb;Server=DESKTOP-II14AQP\\SQLEXPRESS");
-        }
-    }
-
-③ マイグレーション開始
-・Add-Migration AppDatabase　←　マイグレーションクラス名
-
-④ DBServerに反映する
-・Update-Database
-
-⑤ マイグレーション更新
-・Add-Migration AppDatabase2　←　新しいマイグレーションクラス名
-
-⑥ 更新をDBServerに反映する
-・Update-Database AppDatabase2
-
-⑦ DBServerをロールバックする
-・Update-Database AppDatabase2
-
-⑧ マイグレーションを初期化するにはMigrationsフォルダとデータベースを削除して③から始める
-
- */
