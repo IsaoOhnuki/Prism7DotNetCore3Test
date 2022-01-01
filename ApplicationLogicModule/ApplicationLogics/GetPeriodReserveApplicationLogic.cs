30e6d4caf0dbaf085bc2840580f8eef2ba4f72ea
IsaoOhnuki <isa0_net@hotmail.com>
2022-01-01 16:50:30 +0900

    commit

---
+++ ApplicationLogicModule/ApplicationLogics/GetPeriodReserveApplicationLogic.cs
@@ -0,0 +1,18 @@
+﻿using ApplicationLogicModule.BusinessLogics;
+using ModelLibrary.ActionLogic;
+using ModelLibrary.InputModels;
+using ModelLibrary.ResultModels;
+
+namespace ApplicationLogicModule.ApplicationLogics
+{
+    public class GetPeriodReserveApplicationLogic : ApplicationLogicBase<GetPeriodReserveResultModel, GetPeriodReserveInputModel>
+    {
+        protected override GetPeriodReserveResultModel OnExecute(GetPeriodReserveInputModel inputModel)
+        {
+            GetPeriodReserveResultModel resultModel = ActionLogicInvoker<
+                GetPeriodReserveBusinessLogic, GetPeriodReserveResultModel, GetPeriodReserveInputModel>.
+                    Invoke(inputModel);
+            return resultModel;
+        }
+    }
+}
